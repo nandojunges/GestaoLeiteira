a1c06843a83b75dff495ba6f2c4cab493824a3e6
nandojunges <ferjunges@outlook.com>
2025-08-16 12:52:14 -0300

    Update Login.jsx

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { useNavigate, Link } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import api from '../../api';
-import LoginInfoRotativo from '../../auth/LoginInfoRotativo';
+import LoginInfoRotativo from './LoginInfoRotativo';
 
 export default function Login() {
   const [email, setEmail] = useState('');
